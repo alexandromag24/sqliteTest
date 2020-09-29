b5b3eb087c46cf20d4e2f7f0e8bcb6bc2870e83e
Alejandro Perez Rodriguez <alejandro.perez@vra.cubacatering.avianet.cu>
2020-09-29 09:29:14 -0400

    initial commit

---
+++ Models/Blog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sqliteTest.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int BlogId { get; set; }
+        [Required]
+        [MaxLength(128)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string SubTitle { get; set; }
+        [Required]
+        public string DateTimeAdd { get; set; }
+    }
+}
---
+++ Models/MyDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace sqliteTest.Models
+{
+    public class MyDbContext : DbContext
+    {
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=TestDatabase.db", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Blog> Blogs { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Map table names
+            modelBuilder.Entity<Blog>().ToTable("Blogs", "test");
+            modelBuilder.Entity<Blog>(entity =>
+            {
+                entity.HasKey(e => e.BlogId);
+                entity.HasIndex(e => e.Title).IsUnique();
+                entity.Property(e => e.DateTimeAdd);
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
