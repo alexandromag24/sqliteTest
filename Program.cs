3fc88f2a9c255774448f9749b4d79e66085988a1
alejandropr27@nauta.cu <alejandropr27@nauta.cu>
2020-09-29 11:28:36 -0400

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -15,15 +16,19 @@ namespace sqliteTest
     {
         public static void Main(string[] args)
         {
-            
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                 .UseKestrel((context, options) =>
+                 {
+                     var port = Environment.GetEnvironmentVariable("PORT");
+                     if (!string.IsNullOrEmpty(port))
+                     {
+                         options.ListenAnyIP(int.Parse(port));
+                     }
+                 });
     }
 }
